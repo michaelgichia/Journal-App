35061389b35d6efa7ad5f7144d00746f5ce4da74
Michael Gichia <gichuru.gichia@gmail.com>
2025-03-20 15:07:29 +0300

    Add login logic.

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,19 +1,39 @@
 'use server'
 
+import {AuthError, CredentialsSignin} from 'next-auth'
+
 import { IAuthState } from '@/types/auth';
+import {signIn} from '@/config/auth'
 
 export async function authenticate(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _prevState: IAuthState | undefined,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _formData: FormData,
+  formData: FormData,
 ): Promise<IAuthState | undefined> {
   try {
+    await signIn('credentials', {
+      email: formData.get('email'),
+      password: formData.get('password'),
+      redirectTo: '/dashboard',
+    })
     return {
       message: 'Successfully signed in!',
       success: true,
     }
   } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case CredentialsSignin.type:
+          return {
+            message: 'Invalid credentials.',
+            success: false,
+          }
+        default:
+          return {
+            message: 'Something went wrong.',
+            success: false,
+          }
+      }
+    }
     throw error
   }
 }
