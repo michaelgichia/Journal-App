a0459b71599b956cffc339106b012685568a680e
Michael Gichia <gichuru.gichia@gmail.com>
2025-03-21 05:14:30 +0300

    Clean up sidebar.

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -9,7 +9,7 @@ export default function SideNav() {
         <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block md:pe-2 md:pt-10'>
           <Link
             href='/dashboard'
-            className='flex h-[48px] grow items-center justify-center rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-800 md:flex-none md:justify-start md:p-2 md:px-3 bg-sky-100 text-teal-800'
+            className='flex h-[48px] grow items-center justify-center rounded-md bg-teal-100 p-3 text-sm font-medium hover:bg-teal-300 hover:text-teal-800 md:flex-none md:justify-start md:p-2 md:px-3 bg-sky-100 text-teal-800'
           >
             <p className='hidden md:block'>Home</p>
           </Link>
@@ -20,7 +20,7 @@ export default function SideNav() {
             await signOut({redirectTo: '/'})
           }}
         >
-          <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-teal-800 md:flex-none md:justify-start md:p-2 md:px-3 cursor-pointer'>
+          <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-teal-100 p-3 text-sm font-medium hover:bg-teal-300 hover:text-teal-800 md:flex-none md:justify-start md:p-2 md:px-3 cursor-pointer'>
             <Power className='w-6' />
             <div className='hidden md:block'>Sign Out</div>
           </button>
